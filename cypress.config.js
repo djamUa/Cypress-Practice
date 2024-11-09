305cff54ae2d4f3f92bca774838610db7dde5037
Vlad <dzyubenko.vlad@gmail.com>
2024-11-09 21:29:24 +0200

    homeWork19

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Додайте будь-які події, якщо потрібно
     },
+    baseUrl: "https://qauto.forstudy.space/",  // Замініть на URL вашого застосунку
   },
 });
--- cypress/e2e/homeWork18/header-buttons.cy.js
+++ cypress/e2e/homeWork18/header-buttons.cy.js
@@ -1,12 +1,1 @@
-/// <reference types="cypress"/>  
-
-describe('Header Buttons Tests', () => {  
-  it.only('test1', () => {
-    cy.visit('https://guest:welcome2qauto@qauto.forstudy.space/');
-    cy.get ('nav').find('button');
-    cy.get ('.header-link').not('a');
-    cy.get ('.header_signin');
-    cy.contains('Sign up');
-
-  })
-})
+// 
